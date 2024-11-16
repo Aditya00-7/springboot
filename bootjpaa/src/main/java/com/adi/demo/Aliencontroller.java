243e7fe7d6111c6748f70677931298f8116933bf
Aditya Raj <adityaraj21122018@gmail.com>
2024-11-16 17:12:52 +0530

    till part 15 done

--- bootjpaa/src/main/java/com/adi/demo/Aliencontroller.java
+++ bootjpaa/src/main/java/com/adi/demo/Aliencontroller.java
@@ -3,6 +3,7 @@ package com.adi.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.adi.demo.dao.Alienrepo;
@@ -24,18 +25,24 @@ public class Aliencontroller
 	return "home.jsp";	
 	}
 	
-	@RequestMapping("/getAlien")
-	public  ModelAndView getAlien(@RequestParam int aid) 
-	{
-		ModelAndView mv =new ModelAndView("showAlien.jsp");
+	@RequestMapping("/aliens")
+	@ResponseBody
+ //public  ModelAndView getAlien(@RequestParam int aid) 
+	//{
+		//ModelAndView mv =new ModelAndView("showAlien.jsp");
+		//Alien alien = repo.findById(aid).orElse(new Alien());
+		
+		//System.out.println(repo.findByTech("java"));
+		//System.out.println(repo.findByAidGreaterThan(102));
+		//System.out.println(repo.findByTechSorted("Java"));
+		
+		
+		//mv.addObject(alien);
+	//return mv;}
+	
+	public String getAlien(@RequestParam int aid) {
 		Alien alien = repo.findById(aid).orElse(new Alien());
-		
-		System.out.println(repo.findByTech("java"));
-		System.out.println(repo.findByAidGreaterThan(102));
-		System.out.println(repo.findByTechSorted("Java"));
-		
-		
-		mv.addObject(alien);
-	return mv;	
+		return repo.findAll().toString();
 	}
 }
+
